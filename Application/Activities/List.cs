bbabc0acf48f75bd813f0b2e2aab743e7fad2822
Ignas Kavaliauskas <ignas.kavaliauskas5@gmail.com>
2021-03-12 19:21:30 +0200

    Clean up start up class

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -25,8 +25,8 @@ namespace Application.Activities
             }
             
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
-            {
-                return await _context.Activities.ToListAsync();
+            { 
+                return await _context.Activities.ToListAsync(cancellationToken);
             }
         }
     }
