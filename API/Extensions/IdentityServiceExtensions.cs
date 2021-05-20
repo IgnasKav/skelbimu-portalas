e731b13c15fec7ce08430810ad60dde3b4facea6
Ignas Kavaliauskas <ignas.kavaliauskas5@gmail.com>
2021-05-20 14:58:55 +0300

    add Identity

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,20 @@
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddIdentityCore<User>().AddEntityFrameworkStores<DataContext>()
+                .AddSignInManager<SignInManager<User>>();
+            services.AddAuthentication();
+
+            return services;
+        }
+    }
+}
